5a5e51ed1b39a6ab61bce4fe7b1dabd5285fa96c
Aaron Demski <ajdemski@frontier.com>
2023-01-13 11:31:44 -0800

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 // Business Logic
-
-
+function add(q1, q2, q3) {
+    return q1 / q2 / q3;
+  }
+  
 // User Interface Logic
+function handleCalculation(event) {
+    event.preventDefault();
+    let q1 = 1(prompt("q1 will be added to q2 (q1)"))
+    let q2 = 2(prompt("q2 will be added to q3 (q2)"))
+    let q3 = 3(prompt("q3 to be added to q1 (q3)"))
 
+if (q1 + q2 + q3){
+    
+}
+}
