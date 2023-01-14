3cae1141b13ae847eef1f15b1eb3ea2c70d99699
Aaron Demski <ajdemski@frontier.com>
2023-01-13 16:45:32 -0800

    wip-project2

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
-// Business Logic
-function add(q1, q2, q3) {
-    return q1 / q2 / q3;
-  }
-  
-// User Interface Logic
-function handleCalculation(event) {
-    event.preventDefault();
-    let q1 = 1(prompt("q1 will be added to q2 (q1)"))
-    let q2 = 2(prompt("q2 will be added to q3 (q2)"))
-    let q3 = 3(prompt("q3 to be added to q1 (q3)"))
+const myForm = document.querySelector('my-form');
+const answer1 = document.querySelectorAll('answer1, answer12, answer123');
+const answer2 = document.querySelectorAll('answer2, answer23, answer234');
+const answer3 = document.querySelectorAll('answer3, answer34, answer345');
+const suggestedLanguage = document.querySelector('suggestedLanguage');
 
-if (q1 + q2 + q3){
-    
-}
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    if(answer1 === answer12 || answer12 === answer123 || answer123 === answer1)
+    msg.class.List.add('C#')
+    msg.innerHTML = 'C#';
+
+    setTimeout(() =>msg.remove(), 3000);
+
 }
